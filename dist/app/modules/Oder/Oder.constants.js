a47ceada3e2606b2207197679053949820052d4b
unknown <mojahidislammishat@gmail.com>
2025-01-02 02:39:13 +0600

    nodemailer use

---
+++ dist/app/modules/Oder/Oder.constants.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OrderSearchableFields = exports.OrderFilterableFields = void 0;
+exports.OrderFilterableFields = [
+    "searchTerm",
+    "user",
+    "status",
+    "dateOrdered",
+];
+exports.OrderSearchableFields = ["user", "status", "dateOrdered"];
